4ad1f59f37883e52ccf17f80dc680588b8ab25fb
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-02-07 12:02:58 +0400

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+// Movement Animation to happen
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+container.addEventListener('mousemove', (e) => {
+    //console.log(e.pageX, e.pageY)
+    let xAxis = (window.innerWidth/2 - e.pageX)/10;
+    let yAxis = (window.innerHeight/2 - e.pageY)/10;
+
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+})
